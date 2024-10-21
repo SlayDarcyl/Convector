4dbfbc4167b64ef0f88bd530a8d0db2bfbeeeff2
SlayDarcyl <mylesmarqdarcykl@gmail.com>
2024-10-21 07:55:43 +0300

    Create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+    const TaskManager = await hre.ethers.getContractFactory("TaskManager");
+    const taskManager = await TaskManager.deploy();
+    await taskManager.deployed();
+    console.log("TaskManager deployed to:", taskManager.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
